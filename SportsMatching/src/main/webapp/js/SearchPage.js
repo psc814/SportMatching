93a2d5099fa9cd320c1b4dafc000200cd3c43c06
junnnnK <junnnnK@LAPTOP-UMP39SS4>
2019-12-18 17:38:38 +0900

    구장검색 완성!

--- SportsMatching/src/main/webapp/js/SearchPage.js
+++ SportsMatching/src/main/webapp/js/SearchPage.js
@@ -12,12 +12,18 @@ function chg(val){
 		}
 	}
 function search(){
-	$.ajax({
-		type: "POST",
-		url:"./search.do",
-		data: "condition="+searchbar.value,
-		async: true,
-		success: function(msg){
+	var option = $("#searchoption");
+	var condition = $("#searchbar");
+	if(condition.val().trim() == "" || condition.val().trim() == null){
+		condition.focus();
+		alert("검색어를 입력하세요");
+	}else if(option.val() == stadium){
+		$.ajax({
+			type: "POST",
+			url:"./stadiumSearch.do",
+			data: "condition="+searchbar.value,
+			async: true,
+			success: function(msg){
 				$("#tableCase").append("<table id='resultTable'>");
 				$("#tableCase").append("<tr>");
 				$("#tableCase").append("<th class='listNum'>번호</th>");
@@ -27,16 +33,53 @@ function search(){
 				$.each(msg.searchLists, function(i, lists){
 					$("#tableCase").append("<tr>");
 					$("#tableCase").append("<td class='listNum'>"+i+"</td>");
-					$("#tableCase").append("<td class='listName'><a href='#'>"+lists.stadium_name+"</a></td>");
+					$("#tableCase").append("<td class='listName'><a href='./main.do?stadium_code="+lists.stadium_code+"'>"+lists.stadium_name+"</a></td>");
 					$("#tableCase").append("<td class='listAddress'>"+lists.address+"</td>");
 					$("#tableCase").append("</tr>");
 				})
 				$("#tableCase").append("</table>");
-		},
-		error: function(){
-			
-		}
-	})
+			},
+			error: function(){
+				
+			}
+		})	
+	}else if(option == matching){
+		$.ajax({
+			type: "POST",
+			url:"./matchingSearch.do",
+			data: "condition="+searchbar.value,
+			async: true,
+			success: function(msg){
+				$("#tableCase").append("<table id='resultTable'>");
+				$("#tableCase").append("<tr>");
+				$("#tableCase").append("<th class='listNum'>"+lists.team_id+"</th>");
+				$("#tableCase").append("</tr>");
+				$("#tableCase").append("<tr>");
+				$("#tableCase").append("<td class='listNum'>"+lists.playground+"</td>");
+				$("#tableCase").append("</tr>");
+				$("#tableCase").append("<tr>");
+				$("#tableCase").append("<th class='listNum'>"+lists.playground+"</th>");
+				$("#tableCase").append("</tr>");
+				$("#tableCase").append("<th class='listName'>구장명</th>");
+				$("#tableCase").append("<th class='listAddress'>주소</th>");
+				$.each(msg.searchLists, function(i, lists){
+					$("#tableCase").append("<tr>");
+					$("#tableCase").append("<td class='listNum'>"+i+"</td>");
+					$("#tableCase").append("<td class='listName'><a href='./main.do?stadium_code="+lists.stadium_code+"'>"+lists.stadium_name+"</a></td>");
+					$("#tableCase").append("<td class='listAddress'>"+lists.address+"</td>");
+					$("#tableCase").append("</tr>");
+				})
+				$("#tableCase").append("</table>");
+			},
+			error: function(){
+				
+			}
+		})
+	}else if(option == team){
+		
+	}else{
+		
+	}
 		
 	
 }
